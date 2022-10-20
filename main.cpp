7ba90a9559f7bb868ac9184aea02a657f7558c04
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-10-20 02:57:38 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+/*
+ the main fuctionality 
+ 1- menu function
+ 2- add new patient function
+ 3- print all specionalizaiton with patient function
+ 4- get next patient to the doctor
+ 5- exit the system 
+*/
+
+
+
+
+int main(){
+
+    
+    return 0;
+}
