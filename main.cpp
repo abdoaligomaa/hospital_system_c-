639e948227ecf049a9ffc6f7db739c275832deb8
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-10-21 03:32:02 +0200

    menu funcion using recursion

--- main.cpp
+++ main.cpp
@@ -9,11 +9,35 @@ using namespace std;
  5- exit the system 
 */
 
+// menu function
+int menu(){
+    int TheChoice=-1;
+    cout << "Enter you choice" << endl;
+    cout<<"1) Add new patient "<<endl;
+    cout<<"2) Print all patients "<<endl;
+    cout<<"3) Get the next patient "<<endl;
+    cout << "4) Exit " << endl;
+    cout<<"**************************************************"<<endl;
+    cout<<"Your choice is : ";
+    cin>>TheChoice;
+    if (!(TheChoice <= 4 && TheChoice >= 1)){
+        cout<<"Your choice in incorrect!!!!!!, please try again "<<endl;
+        cout << "**************************************************" << endl;
+        return menu();
+    }
+    return TheChoice;
+
+}
+
+void MainProject(){
+    menu();
+}
+
 
 
 
 int main(){
 
-    
+    MainProject();
     return 0;
 }
